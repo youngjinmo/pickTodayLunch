c22c19ab098776678b6ed05a534e09bdc7de8b1f
youngjin <creator.youngjin@gmail.com>
2019-02-20 18:16:47 +0900

    add annotation for each method

--- src/khLunch/goThere.java
+++ src/khLunch/goThere.java
@@ -1,31 +1,37 @@
 package khLunch;
 
-import java.util.Random;
 import java.util.Scanner;
-import khLunch.countTime;
+import khLunch.landingEffect;
 
 public class goThere {
 	
 	public static void main(String[] args) throws InterruptedException {
 
-		countTime ct = new countTime();
-		Random rd = new Random();
-		
-		int go = rd.nextInt(5);
-		
+		// Objects what I used.
+		landingEffect ct = new landingEffect();
 		Scanner sc = new Scanner(System.in);
+		
+		// local variables
 		int boot;
 		
+		// program begins
 		System.out.println("밥고르기 힘들지? 시간도 없는데..\n내가 골라줄테니까 이제 생산적인 고민만 하자.");
-		System.out.println("1. 그래 골라줘~\n2. 고맙지만 사양할게.");
+		// request user a selection
+		ct.selection();		
+		
+		// input user's selection to local variable
 		boot = sc.nextInt();
 		
+		// conditional for execute or quit
 		if(boot==1) {
-			System.out.println("좋았어 기대해~");
+			// if user wants execute program
+			System.out.println("\n좋았어 기대해~");
 			ct.countDown(5);
+			ct.welcome();
 			
 		} else {
-			System.out.println("그래 잘가라.\n비싸고 맛없는 집을 가길 바랄게.");
+			// if user wants quit program
+			System.out.println("\n그래 잘가라.\n비싸고 맛없는 집을 가길 바랄게.");
 		}
 		
 	}
--- src/khLunch/landingEffect.java
+++ src/khLunch/landingEffect.java
@@ -4,33 +4,52 @@ import java.util.concurrent.TimeUnit;
 
 class landingEffect {
 	
+	// request user a selection
 	public void selection() {
 		System.out.print("======================================" + "\nㅐ 1번. 그래 골라줘 || 2번. 괜찮아 사양할게. ㅐ" + "\n======================================"+" \n  너의 선택은?  ");
 	}
 	
+	// show countdown beginning of program as fun
 	public void countDown(int num) throws InterruptedException {
-		// 입력값으로 받은 시간만큼 카운트다운 
+		
+		// initialized variable 'emp'
+		// just for visible effect
 		String emp = "";
+		
+		// printout countdown number with minor effect
 		for(int i=num; i>0; i--) {
 			for(int j=0; j<num; j++) {
 				emp += " ";
 			}
+			// printout
 			System.out.println(emp+(i));
+			
+			// time delay applied to printout for countdown
 			TimeUnit.MICROSECONDS.sleep(900000);
 		}
 	}
 	
+	// just fun
 	public void welcome() throws InterruptedException {
+		
+		// array for storing String variable for each line
 		String[] arr = new String[5];
 		arr[0] = "__        __   _";
 		arr[1] = "\\ \\      / /__| | ___ ___  _ __ ___   ___ ";
 		arr[2] = " \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\";
 		arr[3] = "  \\ V  V /  __/ | (_| (_) | | | | | |  __/";
 		arr[4] = "   \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|";
+		
+		// printout welcome
 		for(int i=0; i<arr.length; i++) {
 			System.out.println(arr[i]);
+			
+			// time delay applied to printout 
+			// for look like classic printer
 			TimeUnit.MICROSECONDS.sleep(500000);
 		}
+		
+		// divide all effects from real program
 		System.out.println("==========================================");
 	}
 }
