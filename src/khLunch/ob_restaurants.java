797235691f1f4da76d5b0bc1ae2e95ce90c8436e
youngjin <creator.youngjin@gmail.com>
2019-02-21 10:54:31 +0900

    add break for each case statement of switch

--- src/khLunch/ob_restaurants.java
+++ src/khLunch/ob_restaurants.java
@@ -6,12 +6,12 @@ public class ob_restaurants {
 		String pick = "";
 		
 		switch(ran) {
-		case 0: pick =  "리틀하노이(쌀국수)";
-		case 1: pick = "역삼735(이자카야)";
-		case 2: pick = "담소사골순대(순대국)";
-		case 3: pick = "봉된장(한정식)";
-		case 4: pick = "명동칼국수";
-		case 5: pick = "조샌드위치(커피)";
+		case 0: pick =  "리틀하노이(쌀국수)"; break;
+		case 1: pick = "역삼735(이자카야)"; break;
+		case 2: pick = "담소사골순대(순대국)"; break;
+		case 3: pick = "봉된장(한정식)"; break;
+		case 4: pick = "명동칼국수"; break;
+		case 5: pick = "조샌드위치(커피)"; break;
 		}
 		
 		return pick;
